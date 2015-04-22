051cbba49b25ccb72748d6af155c4d4e528d4e06
Monshta <frgimena@gmail.com>
2015-04-21 22:31:32 -0700

    v0.1
    -Added Character
    -Added Scrolling Background

---
+++ Space Mans/Assets/Scripts/Alien.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alien : MonoBehaviour {
+
+	public Vector3 alienPos;
+	public Vector3 mousePos;
+
+	void Start () {
+
+	}
+
+	void Update () {
+		mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z + 10));
+		transform.position = mousePos;
+		Debug.Log (mousePos);
+	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "Points") {
+			Debug.Log ("+1 Point!");
+			Destroy (other.gameObject);
+		}
+	}
+}
---
+++ Space Mans/Assets/Scripts/Background.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+	void Update () {
+		MeshRenderer mr = GetComponent<MeshRenderer> ();
+		Material mat = mr.material;
+		Vector2 offset = mat.mainTextureOffset;
+
+		offset.x += Time.deltaTime / 7;
+		mat.mainTextureOffset = offset;
+	}
+}
